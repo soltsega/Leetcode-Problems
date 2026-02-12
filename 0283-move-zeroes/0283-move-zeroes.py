c90dde04e5bc9e2ae87b314a300d5f6829873122
Solomon Tsega <tsegasolomon538@gmail.com>
2026-02-12 23:26:29 +0300

    Time: 3 ms (89.6%), Space: 13.8 MB (16.02%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        insert_pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[insert_pos] = nums[i]
+                insert_pos += 1
+        for i in range(insert_pos, len(nums)):
+            nums[i] = 0
