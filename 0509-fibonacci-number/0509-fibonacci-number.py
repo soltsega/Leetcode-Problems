bd18cc65339bb136305c7e46d789a30595f63eec
Solomon Tsega <tsegasolomon538@gmail.com>
2026-02-14 00:23:01 +0300

    Time: 12 ms (84.17%), Space: 12.5 MB (54.42%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        sequence = [0,1]
+        if n == 0:
+            return sequence[0]
+        if n == 1:
+            return sequence[1]
+
+        for i in range(2, n + 1):
+            next_val = sequence[i - 1] + sequence[i - 2] 
+            sequence.append(next_val)
+        return sequence[n]
+        
