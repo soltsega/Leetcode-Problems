8b7625e5699f9b7a59a3c0f5de29f683ec4ee7a7
Solomon Tsega <tsegasolomon538@gmail.com>
2026-02-14 09:59:17 +0300

    Time: 0 ms (100%), Space: 12.4 MB (86.75%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -0,0 +1,16 @@
+# Technique used: Accumulation / Horner's method
+# Time complexity: O(N)
+# Space complexity: (!)
+
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        result = 0
+        for char in columnTitle:
+            value = ord(char) - ord('A') + 1
+
+            result = result * 26 + value
+        return result
