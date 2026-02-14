7be7cb501e4265ac876d18cdd35b527afcaca29c
Solomon Tsega <tsegasolomon538@gmail.com>
2026-02-14 09:44:51 +0300

    Time: 15 ms (70.21%), Space: 12.9 MB (65.31%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,22 @@
+# Technique used: Two pointer technique
+# Time complexity: O(N)
+# Space complexity: O(1)
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # s.stripe()
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
