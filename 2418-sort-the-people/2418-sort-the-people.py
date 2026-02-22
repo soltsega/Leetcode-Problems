5ec45af7ebdbb450825458907359f3e3e7a1bd4b
Solomon Tsega <tsegasolomon538@gmail.com>
2026-02-23 00:17:14 +0300

    Time: 11 ms (49.31%), Space: 12.7 MB (80.59%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sortPeople(self, names, heights):
+        """
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+        """
+        # Pair names with heights
+        people = list(zip(names, heights))
+        
+        # Sort by height in descending order
+        people.sort(key=lambda x: x[1], reverse=True)
+        
+        # Extract and return the names
+        return [name for name, height in people]
