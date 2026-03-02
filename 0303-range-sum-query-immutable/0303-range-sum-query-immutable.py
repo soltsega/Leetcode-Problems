b7219f128d6bac1b350c3fef10b293633a2455a5
Solomon Tsega <tsegasolomon538@gmail.com>
2026-03-02 20:21:17 +0300

    Time: 10 ms (37.54%), Space: 16.1 MB (63.65%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -1,10 +1,15 @@
+# Technque Used: Prefix Sum
+# Time Complexity: O(N)
+# Space Complexity: O(1)
 class NumArray(object):
 
     def __init__(self, nums):
         """
         :type nums: List[int]
         """
+        # Declare prefix array
         self.prefix = [0] * (len(nums) + 1)
+        # Build prefix array whose length is len(nums) + 1
         for i in range(len(nums)):
             self.prefix[i+1] = self.prefix[i] + nums[i]
 
