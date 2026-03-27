302d32cb01adefb781250c5b95d11b84b9feec8b
Solomon Tsega <tsegasolomon538@gmail.com>
2026-03-27 20:19:51 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int addDigits(int n) {
-        // Base Case: If n is a single digit, we are done
-        if (n < 10) {
-            return n;
-        }
+        
+        if(n<10)
+        return n;
+        int total;
 
-        int total = 0;
-        // Sum the digits of the current number
-        while (n > 0) {
-            total += n % 10;
-            n /= 10;
+        while(n>=0){
+            total += n%10;
+            n/=10;
         }
-
-        // Recursively call the function with the NEW sum
-        return addDigits(total);
+        return addDigits(n);
     }
 };
