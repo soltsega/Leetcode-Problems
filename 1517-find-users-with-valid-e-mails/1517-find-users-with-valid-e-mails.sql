dbe468a252c447be06952262fd8698da70864ad3
Solomon Tsega <tsegasolomon538@gmail.com>
2026-03-29 21:57:10 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,4 +1,5 @@
 -- Write your PostgreSQL query statement below
+-- Technique used: Using regular expression
 SELECT user_id, name, mail
 FROM Users
 WHERE mail ~ '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\.com$';
