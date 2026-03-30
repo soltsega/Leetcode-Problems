06d20f576b2f9ec8cbd295a1a504f7ae79982ebc
Solomon Tsega <tsegasolomon538@gmail.com>
2026-03-30 21:48:31 +0300

    Time: 7 ms (98.92%), Space: 12.3 MB (76.83%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseBits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        result = 0
+        for _ in range(32):
+            result = (result << 1) | (n & 1)
+            n >>= 1
+        return result
