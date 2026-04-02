37bf726d8347692b79189a9bd96bdcbc07219ee9
Solomon Tsega <tsegasolomon538@gmail.com>
2026-04-03 00:54:12 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -1,3 +1,7 @@
+# Technique used: Preorder traversal
+# Time complexity: O(N) where N is the number of nodes in the tree
+# Space complexity: O(H) where H is the height of the tree
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
