0870623ba44b3c756eb97cfda2af4bbb611056cc
Solomon Tsega <tsegasolomon538@gmail.com>
2026-04-03 21:21:20 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -6,6 +6,11 @@
 #         self.right = right
 class Solution(object):
     def mergeTrees(self, root1, root2):
+        """
+        :type root1: Optional[TreeNode]
+        :type root2: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
         if not root1:
             return root2
         if not root2:
@@ -13,7 +18,7 @@ class Solution(object):
 
         root1.val += root2.val
 
-        root1.left = self.mergeTrees(root1.left, root2.left)
         root1.right = self.mergeTrees(root1.right, root2.right)
+        root1.left = self.mergeTrees(root1.left, root2.left)
 
         return root1
