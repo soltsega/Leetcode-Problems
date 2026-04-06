632dc5e0605e66be1551023988596653887f95b6
Solomon Tsega <tsegasolomon538@gmail.com>
2026-04-06 10:25:50 +0300

    Time: 0 ms (100%), Space: 19.3 MB (19.65%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def search(self, nums, target):
+        def helper(left, right):
+            if left > right:
+                return -1
+
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif target < nums[mid]:
+                return helper(left, mid - 1)
+            else:
+                return helper(mid + 1, right)
+
+        return helper(0, len(nums) - 1)
