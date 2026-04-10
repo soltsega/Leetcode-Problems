7cfcc19fa343af6befdd5a122f495e0658e9bd9d
Solomon Tsega <tsegasolomon538@gmail.com>
2026-04-10 10:08:44 +0300

    Time: 1191 ms (12.4%), Space: 19.3 MB (70.09%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,25 +1,8 @@
-class Solution(object):
-    def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        if x < 2:
-            return x
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        n =  0
+        while n*n <= x:
+            n += 1
+
+        return n-1
         
-        left, right = 2, x // 2
-        ans = 1 # Default for x=1, though handled above
-        
-        while left <= right:
-            mid = left + (right - left) // 2
-            num = mid * mid
-            
-            if num == x:
-                return mid
-            elif num < x:
-                ans = mid  # mid is a potential floor, keep searching higher
-                left = mid + 1
-            else:
-                right = mid - 1
-                
-        return ans
