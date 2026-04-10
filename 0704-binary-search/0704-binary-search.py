3a982bdbe084df0639aa962ef621c2fdab912f52
Solomon Tsega <tsegasolomon538@gmail.com>
2026-04-10 09:18:11 +0300

    Time: 0 ms (100%), Space: 20.6 MB (50.4%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,20 +1,15 @@
-# TEchnique used: Recursion
-# Time complexity: O(logn)
-# Space complexity: O(logn)
-
 class Solution(object):
     def search(self, nums, target):
-        def helper(left, right):
-            if left > right:
-                return -1
+        left, right = 0, len(nums) - 1
 
+        while left <= right:
             mid = (left + right) // 2
 
             if nums[mid] == target:
                 return mid
             elif target < nums[mid]:
-                return helper(left, mid - 1)
+                right = mid - 1
             else:
-                return helper(mid + 1, right)
+                left = mid + 1
 
-        return helper(0, len(nums) - 1)
+        return -1
