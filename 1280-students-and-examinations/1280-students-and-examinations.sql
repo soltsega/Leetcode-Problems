f1034ef907407d8c3787bbe2881635078bce6382
Solomon Tsega <tsegasolomon538@gmail.com>
2026-05-10 14:31:22 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -9,5 +9,5 @@ CROSS JOIN Subjects sub
 LEFT JOIN Examinations e 
     ON s.student_id = e.student_id 
     AND sub.subject_name = e.subject_name
-GROUP BY s.student_id, s.student_name, sub.subject_name
+GROUP BY s.student_id, s.student_name
 ORDER BY s.student_id, sub.subject_name;
