461aa4fc43d9203fd56196253af80367e0102859
Solomon Tsega <tsegasolomon538@gmail.com>
2026-05-12 19:54:55 +0300

    Time: 6 ms (91.83%), Space: 20.5 MB (51.65%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        char = list(s)
+        set1 = set("aeiouAEIOU")
+
+        left, right = 0, len(s)-1
+
+        while left < right:
+
+            while left < right and char[left] not in set1:
+                left +=1
+
+            while left < right and char[right] not in set1:
+                right -= 1
+
+            char[left], char[right] = char[right], char[left]
+            left +=1
+            right -=1
+
+        return "".join(char)
+
+
