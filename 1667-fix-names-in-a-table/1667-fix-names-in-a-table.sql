71367bafd86efe80678d17000960ce8faf56dc41
Solomon Tsega <tsegasolomon538@gmail.com>
2026-05-14 16:28:17 +0300

    Time: 830 ms (10.23%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,4 +1,9 @@
+-- Overall time complexity: O(N*K) where N is the number of rows and K is the lenght of each string 
+-- Overall space complexity: O(N*K) where N and K as in the above
+
+
 # Write your MySQL query statement below
-select user_id, concat(upper(left(name, 1)), lower(substring(name, 2))) as name 
+select user_id, concat(upper(left(name, 1)), lower(substring(name, 2))) 
+as name 
 from Users
 order by user_id;
