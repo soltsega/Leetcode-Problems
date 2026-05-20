2eee99ec18cb71b190a7369f080123911ddd2470
Solomon Tsega <tsegasolomon538@gmail.com>
2026-05-20 15:45:08 +0300

    Time: 2 ms (94.59%), Space: 19.1 MB (97.47%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        map_st = {}
+        map_ts = {}
+
+        for tItem, sItem in zip(t,s):
+            if sItem in map_st:
+                if map_st[sItem] != tItem:
+                    return False
+            else:
+                map_st[sItem] = tItem
+            if tItem in map_ts:
+                if map_ts[tItem] != sItem:
+                    return False
+            else:
+                map_ts[tItem] = sItem
+        return True
