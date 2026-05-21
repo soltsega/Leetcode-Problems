a0ffdf1120b15d57316e1ca5ff41caa044e3c4fe
Solomon Tsega <tsegasolomon538@gmail.com>
2026-05-21 14:36:36 +0300

    Time: 0 ms (100%), Space: 19.5 MB (19.65%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,10 +1,15 @@
+# Technique used: Hash Map Frequency Counting
+# Time complexity: O(N) overall
+# Space complexity: O(N) overall
+
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # m, n = len(self.num1), len(self.num2)
+        #Linear time and space complexity 
         counts = Counter(nums1)
 
-
+        # O(N) space
         intersection = []
+        # Linear time complexity
         for i in nums2:
             if counts[i]>0:
                 intersection.append(i)
