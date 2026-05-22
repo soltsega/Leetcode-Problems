90df73236652295ddca31ed4ca992c5ba2c47090
Solomon Tsega <tsegasolomon538@gmail.com>
2026-05-22 15:28:40 +0300

    Time: 11 ms (98.53%), Space: 53.7 MB (9.85%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,3 +1,8 @@
+# Technique used: Two pointer technique
+# Time complexity: O(N)
+# Space compelxity: O(N)
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,12 +10,14 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-
+        # O(N) space complexity
         values = []
         current = head
 
+        # O(N) time complexity
         while current:
             values.append(current.val)
             current = current.next
+            # O(N) time complexity one for inverting and the other O(N) for comparison
         return values == values[::-1]
         
