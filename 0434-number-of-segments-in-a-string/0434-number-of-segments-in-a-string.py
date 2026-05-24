2c4f7ac42e3291b574b01519d3d728ea528ace3c
Solomon Tsega <tsegasolomon538@gmail.com>
2026-05-24 13:56:40 +0300

    Time: 0 ms (100%), Space: 19.3 MB (45.95%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.py
@@ -0,0 +1,13 @@
+# Technique used: 
+# Time complexity: O(N)
+# Space complexity: O(1)
+class Solution:
+    def countSegments(self, s: str) -> int:
+        count = 0
+        for i in range(len(s)):
+            if s[i]!= ' ' and (i ==0 or s[i-1] == ' '):
+                count+=1
+    
+
+        return count
+        
