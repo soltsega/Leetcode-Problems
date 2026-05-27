cc9c862525d34c79a8424838960838acc477e286
Solomon Tsega <tsegasolomon538@gmail.com>
2026-05-27 11:20:25 +0300

    Time: 27 ms (33.41%), Space: 19.5 MB (94.21%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,17 +1,24 @@
+# Technique used: Hash map or frequency table
+# Time complexity: O(M+N)
+# Space complexity:  O(1)
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         if len(ransomNote) > len(magazine):
             return False
+            
+        # Initialize a frequency array for 26 letters
+        char_counts = [0] * 26
         
-        char_count = [0]*26
-
+        # Count characters in magazine
         for char in magazine:
-            char_count[ord(char)- ord('a')] += 1
-        
+            char_counts[ord(char) - ord('a')] += 1
+            
+        # Deduct characters for ransomNote
         for char in ransomNote:
-            index = ord(char)-ord('a')
-            char_count[index] -=1
-
-            if char_count[index]<0:
+            index = ord(char) - ord('a')
+            char_counts[index] -= 1
+            if char_counts[index] < 0:
                 return False
+                
         return True
