4217accca523f06fe706e5f94412ec546ade92ec
Solomon Tsega <tsegasolomon538@gmail.com>
2026-05-28 11:24:31 +0300

    Time: 568 ms (51.9%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
+-- the things i have learnt today
+-- with is used to create a named temporary table
+-- lead() is a method that is used to select a specific records next, nextnext or nth record from now
+-- over - acts as a window that the operator acts on
 with ranked as (
     select 
     num,
