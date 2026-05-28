a4e58561c0a180101cc3a9335edcf9ce7133aa3d
Solomon Tsega <tsegasolomon538@gmail.com>
2026-05-28 10:57:14 +0300

    Time: 1 ms (63.53%), Space: 19.3 MB (61.86%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,18 @@
+# Technique used: Bitwise OR operator
+# Time complexity: O(M+N)~O(2N) 
+# Space complexity: O(1) since we have used for only result variable
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        result = 0
+
+        for char in s:
+            result ^= ord(char)
+        
+        for char in t:
+            result ^= ord(char)
+
+        return chr(result)
+
+
+        
