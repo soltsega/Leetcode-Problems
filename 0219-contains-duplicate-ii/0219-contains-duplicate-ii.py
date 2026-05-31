9eda24636985f2f9bb180814c55961b6cf940172
Solomon Tsega <tsegasolomon538@gmail.com>
2026-05-31 10:50:44 +0300

    Time: 40 ms (24.48%), Space: 32.2 MB (85.31%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,9 +1,23 @@
+# Technique used: Hash Map
+# Time  Complexity: O(N)
+# Space complexity: O(N)
+
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        index_map = {}
+        """ index_map = {}
 
         for i, val in enumerate(nums):
             if val in index_map and i-index_map[val]<=k:
                 return True
             index_map[val]=i
+        return False """
+
+        window = set()
+
+        for i in range(len(nums)):
+            if nums[i] in window:
+                return True
+            window.add(nums[i])
+            if len(window)>k:
+                window.remove(nums[i-k])
         return False
