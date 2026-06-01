6339002525e188e3dd2269b2237f761bec8e117b
Solomon Tsega <tsegasolomon538@gmail.com>
2026-06-01 23:20:48 +0300

    Time: 0 ms (100%), Space: 19.1 MB (91.94%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.py
@@ -0,0 +1,15 @@
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num == 0:
+            return "0"
+
+        hex_chars = "0123456789abcdef"
+        num &= 0xffffffff
+
+        res = []
+
+        while num:
+            res.append(hex_chars[num & 0xf])
+            num >>= 4
+
+        return "".join(reversed(res))
