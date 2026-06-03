812efd58981292de1d379cbb379a98a57759186e
Solomon Tsega <tsegasolomon538@gmail.com>
2026-06-03 16:48:56 +0300

    Time: 0 ms (100%), Space: 19.4 MB (86.79%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,17 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        output = []
+        
+        # Start at 1 and include n
+        for i in range(1, n + 1):
+            # Check the most strict condition first
+            if i % 3 == 0 and i % 5 == 0:  # or: if i % 15 == 0:
+                output.append("FizzBuzz")
+            elif i % 3 == 0:
+                output.append("Fizz")
+            elif i % 5 == 0:
+                output.append("Buzz")
+            else:
+                output.append(str(i)) # Convert to string type
+                
+        return output
