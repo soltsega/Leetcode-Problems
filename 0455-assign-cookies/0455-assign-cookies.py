e39b6691b2a0aa960df38e99e85dff7384f81e4e
Solomon Tsega <tsegasolomon538@gmail.com>
2026-06-08 05:31:09 +0300

    Time: 26 ms (35.23%), Space: 21.9 MB (86.81%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,11 +1,16 @@
+# Technique used: Two pointer technique
+# Time complexity: O(NlogN+MlogM+M)
+# Space complexity: O(1)
+
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        g.sort()
-        s.sort()
+        g.sort()  #O(NlogN)
+        s.sort()  #O(MlogM)
 
         child = 0
         cookie = 0
 
+        #O(M) since it is two pointer
         while cookie<len(s) and child<len(g):
             if s[cookie]>=g[child]:
                 child+=1
