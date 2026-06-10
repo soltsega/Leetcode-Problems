0bb13f4a2f0a3438488063b02a11217606648998
Solomon Tsega <tsegasolomon538@gmail.com>
2026-06-10 16:40:10 +0300

    Time: 1 ms (11.62%), Space: 19.4 MB (9.93%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -1,9 +1,13 @@
+# Technique used: XOR + Brian Kernighan's Algorithm
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         xor = x^y
         count=0
 
-        while xor!=0:
-            count+=xor&1
-            xor>>=1
+        while xor:
+            xor&=xor-1
+            count+=1
         return count
