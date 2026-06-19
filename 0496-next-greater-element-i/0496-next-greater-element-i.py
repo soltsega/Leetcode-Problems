6b6f425b72786819223af23c7b5ff6c89e3581b6
Solomon Tsega <tsegasolomon538@gmail.com>
2026-06-19 23:29:17 +0300

    Time: 0 ms (100%), Space: 19.2 MB (98.86%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,3 +1,7 @@
+# Technique used: Monotonic Decreasing stack
+# Time complexity: O(M+N) where M = len(nums1) and N = len(nums2)
+# Space complexity: O(N) + O(N) where N = len(nums2) one for hash map and one for the stack
+
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack = []
