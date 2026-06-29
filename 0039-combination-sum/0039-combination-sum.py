a92fd2db5d547af246ae22708e62b48160b5a368
Solomon Tsega <tsegasolomon538@gmail.com>
2026-06-29 15:42:15 +0300

    Time: 13 ms (26.35%), Space: 19.7 MB (31.91%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,18 +1,28 @@
+# Technique used: Backtracking(DFS)
+# Time complexity: O(2^T/M) where T is the target and M is the minimum value in the candidates
+# Space complexity: O(T/M) where T and M mean as shown above
+
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
+        res = [] #global variable
 
         def dfs(i, cur, total):
+            # base case I
             if total == target:
                 res.append(cur.copy())
                 return
+            # base case II
             if i>= len(candidates) or total>target:
                 return
-
+            
+            # append it to the current collection if illegible
             cur.append(candidates[i])
+            # and passing the same index back to the function to iterate over
             dfs(i, cur, total+candidates[i])
-
+            
+            # Cleaning up the current collection
             cur.pop()
+            # walking through the other branch of the tree
             dfs(i+1, cur, total)
 
         dfs(0, [], 0)
