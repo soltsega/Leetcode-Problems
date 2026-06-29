92c5737da2af7cc2d5ea7c67e452c2b8a39ce1a1
Solomon Tsega <tsegasolomon538@gmail.com>
2026-06-29 10:48:31 +0300

    Time: 7 ms (52.82%), Space: 20.4 MB (52.05%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -1,15 +1,21 @@
+# Technique used: Array traversal, hash map, and sorting
+# Time complexity: O(N+N+NlogN+N) = O(NlogN)
+# Time complexity: O(N)+O(N) O(N) = O(N)
+
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
         dic = {}
-        a = score[::]
-        a.sort(reverse=True)
-        ans = []
+        a = score[::]  #O(N) space
+        a.sort(reverse=True)  # O(NlogN) time
+        ans = [] # O(N) space
 
         j = 1
+        # O(N) time
         for i in range(len(a)):
             # if a[i] not in dic:
                 dic[a[i]] = j
                 j+=1
+        # O(N) time
         for i in score:
             if dic[i]==1:
                 ans.append("Gold Medal")
