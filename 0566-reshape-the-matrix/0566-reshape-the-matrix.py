a33848cf809e7186e11ef865e6c4875cf264e5a6
Solomon Tsega <tsegasolomon538@gmail.com>
2026-07-02 12:36:57 +0300

    Time: 0 ms (100%), Space: 19.9 MB (30.01%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,17 @@
+# Technique used:
+# Time complexity:
+# Space complexity:
+
+class Solution:
+    def matrixReshape(self, mat, r, c):
+        m, n = len(mat), len(mat[0])
+
+        if m * n != r * c:
+            return mat
+
+        res = [[0] * c for _ in range(r)]
+
+        for i in range(m * n):
+            res[i // c][i % c] = mat[i // n][i % n]
+
+        return res
