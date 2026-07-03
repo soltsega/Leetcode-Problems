93669a3c21c9d537da5ff48b2178238d5f13b84b
Solomon Tsega <tsegasolomon538@gmail.com>
2026-07-03 13:46:06 +0300

    Time: 0 ms (100%), Space: 19.5 MB (33.35%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,26 +1,19 @@
+
+
+
 class Solution:
-    def permute(self, nums: list[int]) -> list[list[int]]:
-        result = []
-        
-        # Base case: if there's only one element, return it as the only permutation
-        if len(nums) == 1:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        if len(nums)==1:
             return [nums.copy()]
-            
+
         for i in range(len(nums)):
-            # Remove the first element to find permutations of the remaining elements
             n = nums.pop(0)
-            
-            # Recursively find permutations of the sub-array
             perms = self.permute(nums)
-            
-            # Append the removed element back to all permutations of the sub-array
+
             for perm in perms:
                 perm.append(n)
-            
-            # Add the generated permutations to our final result
-            result.extend(perms)
-            
-            # Add the element back to the end of nums to restore the state (backtrack)
+            res.extend(perms)
             nums.append(n)
-            
-        return result
+        return res 
+        
