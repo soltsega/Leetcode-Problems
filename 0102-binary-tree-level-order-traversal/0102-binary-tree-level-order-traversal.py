13e1f7489318701987e6740822028fb5b15d2b66
Solomon Tsega <tsegasolomon538@gmail.com>
2026-07-09 14:39:27 +0300

    Time: 0 ms (100%), Space: 19.9 MB (88.98%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,3 +1,7 @@
+# Technique used: DFS
+# Time complexity: O(N) 
+# Space complexity: O(N) 
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
